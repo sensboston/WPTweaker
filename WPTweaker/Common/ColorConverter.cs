aabfa3b236e1396a1a252ab75ab27ebda6e3b9cc
sensboston <sens.boston@gmail.com>
2015-07-07 10:51:32 -0400

    added new class

---
+++ WPTweaker/Common/ColorConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace System.Windows.Media
+{
+    public static class ColorExtensions
+    {
+        public static int ToArgb(this Color color)
+        {
+            int argb = color.A << 24;
+            argb += color.R << 16;
+            argb += color.G << 8;
+            argb += color.B;
+            return argb;
+        }
+        public static Color FromArgb(int intValue)
+        {
+            return Color.FromArgb(0xFF, (byte)(intValue & 0xFF), (byte)((intValue & 0xFF00) >> 8), (byte)((intValue & 0xFF0000) >> 16));
+        }
+    }
+}
