261b27d4bc4fc23fbe87048b210a12def8f44567
SeNS <sens.boston@gmail.com>
2015-07-08 16:57:20 -0400

    added WebBrowser-based xml editor (much better performance and stability!)

--- WPTweaker/XmlEditorPage.xaml.cs
+++ WPTweaker/XmlEditorPage.xaml.cs
@@ -16,14 +16,32 @@ namespace WPTweaker
 {
     public partial class XmlEditorPage : PhoneApplicationPage
     {
+        const string HtmlTemplate =
+@"<html>
+<head>
+    <meta name='viewport' content='width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no;' />
+    <script type='text/javascript'>
+        function setContent(content) { document.getElementById('editor').innerText = content; }
+        function getContent() { return document.getElementById('editor').innerText; }
+        function setFontSize(size) { document.getElementById('editor').style.fontSize = size+'%'; }
+    </script>
+</head>
+<body>
+    <div style='width:2000px; overflow-x:scroll; overflow-y:scroll; font-family:monospace; font-size:80%' id='editor' contenteditable='true' ></div>
+</body>
+</html>";
+
         AppSettings _settings = new AppSettings();
         string _originalXML = string.Empty;
+        int _fontSize = 80;
 
         public XmlEditorPage()
         {
             InitializeComponent();
             BackKeyPress += XmlEditorPage_BackKeyPress;
-            Editor.Text = _originalXML = _settings.XmlTweaks;
+            _originalXML = _settings.XmlTweaks;
+            webBrowser.Navigated += (_,__) => { webBrowser.InvokeScript("setContent", _originalXML); };
+            webBrowser.NavigateToString(HtmlTemplate);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
@@ -34,7 +52,7 @@ namespace WPTweaker
 
         void XmlEditorPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            string xml = Editor.Text;
+            string xml = (string) webBrowser.InvokeScript("getContent");
             if (!_settings.XmlTweaks.Equals(xml))
             {
                 if (MessageBox.Show("Save changes?", "Warning", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
@@ -55,12 +73,12 @@ namespace WPTweaker
 
         private void ReloadButton_Click(object sender, EventArgs e)
         {
-            Editor.Text = _settings.XmlTweaks;
+            webBrowser.InvokeScript("setContent", _settings.XmlTweaks);
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            string xml = Editor.Text;
+            string xml = (string) webBrowser.InvokeScript("getContent");
             try
             {
                 XDocument doc = XDocument.Parse(xml);
@@ -71,5 +89,23 @@ namespace WPTweaker
                 MessageBox.Show(error.Message);
             }
         }
-    }
+
+       private void IncreaseFontSizeButton_Click(object sender, EventArgs e)
+        {
+            if (_fontSize < 140)
+            {
+                _fontSize += 20;
+                webBrowser.InvokeScript("setFontSize", _fontSize.ToString());
+            }
+        }
+
+        private void DecreaseFontSizeButton_Click(object sender, EventArgs e)
+        {
+            if (_fontSize > 80)
+            {
+                _fontSize -= 20;
+                webBrowser.InvokeScript("setFontSize", _fontSize.ToString());
+            }
+        }
+     }
 }
